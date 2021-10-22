ed7ede06fc8a94f2f8f970f037f268cd838d0cc7
huxiaoou <516984451@qq.com>
2021-10-22 14:40:38 +0800

    Add max-drawdown-duration algorithm. Add max-recover-duration algorithm. Rename algorithm for max-drawdown to max-drawdown-scale.

--- riften.py
+++ riften.py
@@ -45,14 +45,18 @@ class CNAV(object):
         self.m_hold_period_return: float = 0
         self.m_annual_return: float = 0
         self.m_sharpe_ratio: float = 0
-        self.m_max_drawdown: float = 0  # a non negative float
-        self.m_max_drawdown_date: str = self.m_nav_srs.index[0]
 
-        # secondary
-        self.m_max_drawdown_prev_high_date: str = ""
-        self.m_max_drawdown_re_break_date: str = ""
-        self.m_max_drawdown_duration: dict = {"natural": 0, "trade": 0}
-        self.m_max_drawdown_recover_duration: dict = {"natural": 0, "trade": 0}
+        # secondary - A max drawdown scale
+        self.m_max_drawdown_scale: float = 0  # a non negative float, multiplied by RETURN_SCALE
+        self.m_drawdown_scale_srs: pd.Series = pd.Series(data=0.0, index=self.m_nav_srs.index)
+
+        # secondary - B max drawdown duration
+        self.m_max_drawdown_duration: int = 0  # a non negative int, stands for the duration of drawdown
+        self.m_drawdown_duration_srs: pd.Series = pd.Series(data=0, index=self.m_nav_srs.index)
+
+        # secondary - C max recover duration
+        self.m_max_recover_duration: int = 0
+        self.m_recover_duration_srs: pd.Series = pd.Series(data=0, index=self.m_nav_srs.index)
 
     def cal_return_mean(self):
         self.m_return_mean = self.m_rtn_srs.mean()
@@ -80,56 +84,49 @@ class CNAV(object):
         self.m_sharpe_ratio = mu / sd * np.sqrt(self.m_annual_factor)
         return 0
 
-    def cal_max_drawdown(self):
-        nav_hist_high = self.m_nav_srs.iloc[0]
-        self.m_max_drawdown = 0
-        for trade_date in self.m_nav_srs.index:
-            nav_val = self.m_nav_srs[trade_date]
-            if nav_val > nav_hist_high:
-                nav_hist_high = nav_val
-            new_drawdown = (1 - nav_val / nav_hist_high) * RETURN_SCALE  # relative way
-            if new_drawdown >= self.m_max_drawdown:
-                self.m_max_drawdown = new_drawdown
-                self.m_max_drawdown_date = trade_date
-
-        prev_mdd_srs: pd.Series = self.m_nav_srs[self.m_nav_srs.index < self.m_max_drawdown_date]  # nav series before max drawdown date
-        this_mdd_srs: pd.Series = self.m_nav_srs[self.m_nav_srs.index >= self.m_max_drawdown_date]  # nav series after max drawdown date
-        self.m_max_drawdown_prev_high_date = prev_mdd_srs.idxmax()
-        prev_high_value = prev_mdd_srs.max()
-        re_break_idx = this_mdd_srs >= prev_high_value
-        if any(re_break_idx):
-            self.m_max_drawdown_re_break_date = this_mdd_srs[re_break_idx].index[0]
-        else:
-            self.m_max_drawdown_re_break_date = "--"
+    def cal_max_drawdown_scale(self):
+        self.m_drawdown_scale_srs = 1 - self.m_nav_srs / self.m_nav_srs.cummax()
+        self.m_max_drawdown_scale = self.m_drawdown_scale_srs.max()
         return 0
 
-    def cal_mdd_duration(self, t_calendar: CCalendar):
-        # mdd duration
-        _head_date = self.m_max_drawdown_prev_high_date
-        _tail_date = self.m_max_drawdown_date
-        self.m_max_drawdown_duration["trade"] = int(t_calendar.get_sn(_tail_date) - t_calendar.get_sn(_head_date))
-        self.m_max_drawdown_duration["natural"] = int((dt.datetime.strptime(_tail_date, "%Y%m%d") - dt.datetime.strptime(_head_date, "%Y%m%d")).days)
-
-        # recover duration
-        # days before the nav is beyond the prev high since the mdd happens
-        if self.m_max_drawdown_re_break_date == "--":
-            self.m_max_drawdown_recover_duration["trade"] = "--"
-            self.m_max_drawdown_recover_duration["natural"] = "--"
-        else:
-            _head_date = self.m_max_drawdown_date
-            _tail_date = self.m_max_drawdown_re_break_date
-            self.m_max_drawdown_recover_duration["trade"] = int(t_calendar.get_sn(_tail_date) - t_calendar.get_sn(_head_date))
-            self.m_max_drawdown_recover_duration["natural"] = int((dt.datetime.strptime(_tail_date, "%Y%m%d") - dt.datetime.strptime(_head_date, "%Y%m%d")).days)
+    def cal_max_drawdown_duration(self):
+        prev_high = self.m_nav_srs.iloc[0]
+        prev_high_loc = 0
+        prev_drawdown_scale = 0.0
+        drawdown_loc = 0
+        for i, nav_i in enumerate(self.m_nav_srs):
+            if nav_i >= prev_high:
+                prev_high = nav_i
+                prev_high_loc = i
+                prev_drawdown_scale = 0
+            drawdown_scale = 1 - nav_i / prev_high
+            if drawdown_scale > prev_drawdown_scale:
+                prev_drawdown_scale = drawdown_scale
+                drawdown_loc = i
+            self.m_drawdown_duration_srs.iloc[i] = drawdown_loc - prev_high_loc
+        self.m_max_drawdown_duration = self.m_drawdown_duration_srs.max()
         return 0
 
-    def cal_all_indicators(self, t_calendar: CCalendar, t_method: str = "linear"):
+    def cal_max_recover_duration(self):
+        prev_high = self.m_nav_srs.iloc[0]
+        prev_high_loc = 0
+        for i, nav_i in enumerate(self.m_nav_srs):
+            if nav_i >= prev_high:
+                self.m_recover_duration_srs.iloc[i] = 0
+                prev_high = nav_i
+                prev_high_loc = i
+            else:
+                self.m_recover_duration_srs.iloc[i] = i - prev_high_loc
+        self.m_max_recover_duration = self.m_recover_duration_srs.max()
+        return
+
+    def cal_all_indicators(self, t_method: str = "linear"):
         self.cal_return_mean()
         self.cal_return_std()
         self.cal_hold_period_return()
         self.cal_annual_return(t_method=t_method)
         self.cal_sharpe_ratio()
-        self.cal_max_drawdown()
-        self.cal_mdd_duration(t_calendar=t_calendar)
+        self.cal_max_drawdown_scale()
         return 0
 
     def to_dict(self, t_type: str):
@@ -145,14 +142,9 @@ class CNAV(object):
                 "hold_period_return": "{:.2f}".format(self.m_hold_period_return),
                 "annual_return": "{:.2f}".format(self.m_annual_return),
                 "sharpe_ratio": "{:.2f}".format(self.m_sharpe_ratio),
-                "max_drawdown": "{:.2f}".format(self.m_max_drawdown),
-                "prev_high_date": self.m_max_drawdown_prev_high_date,
-                "max_drawdown_date": self.m_max_drawdown_date,
-                "mdd_duration_t": self.m_max_drawdown_duration["trade"],
-                "mdd_duration_n": self.m_max_drawdown_duration["natural"],
-                "re_break_date": self.m_max_drawdown_re_break_date,
-                "recover_duration_t": self.m_max_drawdown_recover_duration["trade"],
-                "recover_duration_n": self.m_max_drawdown_recover_duration["natural"],
+                "max_drawdown_scale": "{:.2f}".format(self.m_max_drawdown_scale),
+                "max_drawdown_duration": "{:d}".format(self.m_max_drawdown_duration),
+                "max_recover_duration": "{:d}".format(self.m_max_recover_duration),
             }
         elif t_type == "chs":
             d = {
@@ -161,12 +153,9 @@ class CNAV(object):
                 "持有期收益": "{:.2f}".format(self.m_hold_period_return),
                 "年化收益": "{:.2f}".format(self.m_annual_return),
                 "夏普比率": "{:.2f}".format(self.m_sharpe_ratio),
-                "最大回撤": "{:.2f}".format(self.m_max_drawdown),
-                "最大回撤开始日期": self.m_max_drawdown_prev_high_date,
-                "最大回撤持续时间": self.m_max_drawdown_duration["trade"],
-                "最大回撤结束日期": self.m_max_drawdown_date,
-                "恢复前高时间": self.m_max_drawdown_recover_duration["trade"],
-                "恢复前高日期": self.m_max_drawdown_re_break_date,
+                "最大回撤": "{:.2f}".format(self.m_max_drawdown_scale),
+                "最长回撤期": "{:d}".format(self.m_max_drawdown_duration),
+                "最长恢复期": "{:d}".format(self.m_max_recover_duration),
             }
         else:
             d = {}
@@ -176,7 +165,61 @@ class CNAV(object):
         print("| HPR = {:>7.4f} | AnnRtn = {:>7.4f} | MDD = {:>7.2f} | SPR = {:>7.4f} | ".format(
             self.m_hold_period_return,
             self.m_annual_return,
-            self.m_max_drawdown,
+            self.m_max_drawdown_scale,
             self.m_sharpe_ratio,
         ))
         return 0
+
+
+# class CNAVEnhanced(CNAV):
+#     def __init__(self, t_raw_nav_srs: pd.Series, t_annual_rf_rate: float, t_freq: str):
+#         super().__init__(t_raw_nav_srs, t_annual_rf_rate, t_freq)
+#
+#         self.m_max_drawdown_scale_date: str = self.m_nav_srs.index[0]
+#         self.m_max_drawdown_scale_prev_high_date: str = ""
+#         self.m_max_drawdown_scale_re_break_date: str = ""
+#         self.m_max_drawdown_scale_duration: dict = {"natural": 0, "trade": 0}
+#         self.m_max_drawdown_scale_recover_duration: dict = {"natural": 0, "trade": 0}
+#
+#     def cal_mdd_duration(self, t_calendar: CCalendar):
+#         # mdd duration
+#         _head_date = self.m_max_drawdown_scale_prev_high_date
+#         _tail_date = self.m_max_drawdown_scale_date
+#         self.m_max_drawdown_scale_duration["trade"] = int(t_calendar.get_sn(_tail_date) - t_calendar.get_sn(_head_date))
+#         self.m_max_drawdown_scale_duration["natural"] = int((dt.datetime.strptime(_tail_date, "%Y%m%d") - dt.datetime.strptime(_head_date, "%Y%m%d")).days)
+#
+#         # recover duration
+#         # days before the nav is beyond the prev high since the mdd happens
+#         if self.m_max_drawdown_scale_re_break_date == "--":
+#             self.m_max_drawdown_scale_recover_duration["trade"] = "--"
+#             self.m_max_drawdown_scale_recover_duration["natural"] = "--"
+#         else:
+#             _head_date = self.m_max_drawdown_scale_date
+#             _tail_date = self.m_max_drawdown_scale_re_break_date
+#             self.m_max_drawdown_scale_recover_duration["trade"] = int(t_calendar.get_sn(_tail_date) - t_calendar.get_sn(_head_date))
+#             self.m_max_drawdown_scale_recover_duration["natural"] = int((dt.datetime.strptime(_tail_date, "%Y%m%d") - dt.datetime.strptime(_head_date, "%Y%m%d")).days)
+#         return 0
+#
+#     def to_dict(self, t_type: str):
+#         d = super().to_dict(t_type)
+#         if t_type == "eng":
+#             d.update({
+#                 "prev_high_date": self.m_max_drawdown_scale_prev_high_date,
+#                 "max_drawdown_date": self.m_max_drawdown_scale_date,
+#                 "mdd_duration_t": self.m_max_drawdown_scale_duration["trade"],
+#                 "mdd_duration_n": self.m_max_drawdown_scale_duration["natural"],
+#                 "re_break_date": self.m_max_drawdown_scale_re_break_date,
+#                 "recover_duration_t": self.m_max_drawdown_scale_recover_duration["trade"],
+#                 "recover_duration_n": self.m_max_drawdown_scale_recover_duration["natural"],
+#             })
+#         elif t_type == "chs":
+#             d.update({
+#                 "最大回撤开始日期": self.m_max_drawdown_scale_prev_high_date,
+#                 "最大回撤持续时间": self.m_max_drawdown_scale_duration["trade"],
+#                 "最大回撤结束日期": self.m_max_drawdown_scale_date,
+#                 "恢复前高时间": self.m_max_drawdown_scale_recover_duration["trade"],
+#                 "恢复前高日期": self.m_max_drawdown_scale_re_break_date,
+#             })
+#         else:
+#             pass
+#         return d
