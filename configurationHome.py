f3d32ec7fec8ba9870b1eb1726d4c8c47a8ae88c
huxiaoou <516984451@qq.com>
2023-05-23 13:40:18 +0800

    Refactor to make it can be installed

--- configurationHome.py
+++
@@ -1,5 +0,0 @@
-import os
-
-SKYRIM_CONST_CALENDAR_PATH = os.path.join("C:\\", "Users", "huxia", "OneDrive", "文档", "Trading", "Database", "Calendar", "cne_calendar.csv")
-SKYRIM_CONST_CALENDAR_DIR = os.path.join("D:\\", "DataBaseCTP")
-SKYRIM_CONST_INSTRUMENT_INFO_PATH = os.path.join("C:\\", "Users", "huxia", "OneDrive", "文档", "Trading", "Database", "Futures", "InstrumentInfo.xlsx")
